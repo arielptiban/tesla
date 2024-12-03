2971712816eb973e3397dcd194df7054f417c943
Ariel <user@MacBook-Pro-de-Ariel.local>
2024-12-03 12:08:22 -0500

    Responsive design y animaciones del menú desplegable

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -7,5 +8,5 @@ export default {
 		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [animations],
 }
